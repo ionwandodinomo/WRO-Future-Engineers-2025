41932431d6a3bfb4d3dcd0f770868fa78411c78a
Sunni Xue <sunni.xue@gmail.com>
2025-07-05 17:33:54 -0400

    more hsv ranges

--- src/testing-structure-nodes/real/values.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-LOWER_BLACK_THRESHOLD = np.array([0, 0, 0])
-UPPER_BLACK_THRESHOLD = np.array([180, 255, 62])
-
-LOWER_BLUE = np.array([107, 88, 0])
-UPPER_BLUE = np.array([121, 255, 211])
---
+++ src/testing-structure-nodes/values.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+LOWER_BLACK_THRESHOLD = np.array([0, 0, 0])
+UPPER_BLACK_THRESHOLD = np.array([180, 255, 62])
+
+LOWER_BLUE = np.array([107, 88, 0])
+UPPER_BLUE = np.array([121, 255, 211])
+
+LOWER_ORANGE1 = np.array([0, 101, 169])
+UPPER_ORANGE1 = np.array([27, 219, 255])
+
+LOWER_RED_THRESHOLD1 = np.array([0, 0, 0])
+UPPER_RED_THRESHOLD1 = np.array([0, 0, 0])
+LOWER_RED_THRESHOLD2 = np.array([163, 120, 138]) 
+UPPER_RED_THRESHOLD2 = np.array([180, 255, 255]) 
+
+LOWER_GREEN_THRESHOLD = np.array([71, 82, 75])
+UPPER_GREEN_THRESHOLD = np.array([111, 255, 178])
+
+LOWER_MAGENTA_THRESHOLD1 = np.array([0, 0, 0])
+UPPER_MAGENTA_THRESHOLD1 = np.array([0, 0, 0])
+LOWER_MAGENTA_THRESHOLD2 = np.array([137, 168, 173])
+UPPER_MAGENTA_THRESHOLD2 = np.array([167, 255, 255])
